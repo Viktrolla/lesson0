14fe390deb8ef25e4141ecf9d1f38a2cc8f87280
Viktrolla <viktoria.kozlovskaya22@gmail.com>
2024-11-28 21:24:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+#1stprogram
+print (9**0.5*5)
+#2ndprogram
+print(9.99>9.98 and 1000 != 1000.1)
+#3rdprogram
+print (2*2+2)
+print (2*(2+2))
+print (2*2+2 == 2*(2+2))
+#4thprogram
+print (int (float('123.456')*10) %10)
